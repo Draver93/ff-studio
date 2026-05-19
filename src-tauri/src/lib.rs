f6cac4cd3e82aa7f02c87d1336b7c5c53bd25c64
Draver93 <alexeyf.160620@gmail.com>
2026-05-19 21:20:58 +0300

    Fixed: tray ctx on lmb

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -46,6 +46,7 @@ pub fn run() {
             let tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
+                .show_menu_on_left_click(false)
                 .on_menu_event(|app, event| match event.id.as_ref() {
                     "show" => {
                         if let Some(window) = app.get_webview_window("main") {
@@ -57,7 +58,7 @@ pub fn run() {
                     _ => {}
                 })
                 .on_tray_icon_event(|tray, event| {
-                    if let TrayIconEvent::DoubleClick { .. } = event {
+                    if let TrayIconEvent::Click { button: tauri::tray::MouseButton::Left, .. } = event {
                         if let Some(window) = tray.app_handle().get_webview_window("main") {
                             let _ = window.show();
                             let _ = window.set_focus();
